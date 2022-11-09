c5912044590dda4826f33d284bff231580df1893
Nathaniel Berube <Rexar14@gmail.com>
2022-11-09 16:15:41 -0600

    Created base page for entries

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -4,7 +4,17 @@ from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
 
+
+def entry(request, title):
+    page_contents = util.get_entry(title)
+    if not page_contents:
+        template = "encyclopedia/entry_error.html"
+    else:
+        template = "encyclopedia/entry.html"
+    return render(
+        request,
+        template,
+        {"title": title, "contents": page_contents},
+    )
