c7d45d31d63d8e162f1906f605581827aefcf86e
Nathaniel Berube <Rexar14@gmail.com>
2022-11-16 19:17:57 -0600

    Updated url for index

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("index", views.index, name="index"),
     path("search", views.search, name="search"),
     path("new_page", views.new_page, name="new_page"),
     path("wiki/random", views.random_page, name="random"),
